4d844062432cfd89d83354e6fdc10aea0fe87b99
natthanan6780903 <natthanan.jay@student.mahidol.edu>
2025-11-18 03:57:23 +0000

    finish

--- stack.h
+++ stack.h
@@ -12,35 +12,55 @@ public:
     ~Stack();
 };
 
-
 void Stack::push(int x){
-  NodePtr new_node=new NODE(x);
+    //push = add a new node to the top of stack
+    //new node created and added to the top
+
+  NodePtr new_node=new NODE(x); //create new node
+
   if(new_node){
-            // Left missing for exercises…
+     new_node->set_next(top); //link new node to current top
+     top = new_node; //update top to point to the new node
+     size++;
    }
- 
-         // Left missing for exercises…
-    
+   else{
+       cout<<"Stack push error: memory allocation failed"<<endl;
+   }
+   
 }
 
 int Stack::pop(){
-        NodePtr t=top;
-        int value;
-        value=t->get_value();
-    // Left missing part for exercises
-        delete t;
-        return value;
+    //pop = remove the top node from stack and return/show/pop its value
+        NodePtr t=top;//temp pointer to hold the top node
+
+        if(t!=NULL){
+            int value; //to hold the value to return
+            value=t->get_value(); //get the value from the top node
+            top = t-> get_next();//update top to point to the next node
+            --size; // decrease size first then show new size
+            delete t; //delete the old top node
+
+            //cout<<"Popped "<<value<<endl;
+            return value; //return the value
+        }
+    cout<<"empty stack"<<endl;
+    return 0;
 	//be careful of the empty stack!!!
     }
 
 Stack::Stack(){
-    //initialize stack
+    //constructor
+    top = NULL; //initially empty stack
+    size=0; 
     
 }
 Stack::~Stack(){
-    //delete all remaning stack (i.e. pop all) 
-    
+     //delete all remaning stack (i.e. pop all) 
+     cout<<"clearing stack..."<<endl;
+     int n = size;
+    while(n>0){
+        pop();
+        n--;
+    }   
 }
-
-
 #endif
