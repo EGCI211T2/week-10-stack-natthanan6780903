896c8919dd4c4db5431c9656d85edcc2a5567307
natthanan6780903 <natthanan.jay@student.mahidol.edu>
2025-11-18 03:57:45 +0000

    finish

--- main.cpp
+++ main.cpp
@@ -1,28 +1,23 @@
 #include<iostream>
 using namespace std;
-
+#include <cstring>
 #include "stack.h"
 
 int main(int argc, char **argv){
     Stack s;
-     s.push(5);
-      s.push(1);
-      s.push(7);
-      s.pop();
-      s.push(6);
-      s.pop();
-
- /*
-  Exercise 1*/
-    /*
+    /*s.push(5); //5
+      s.push(1); //1 5
+      s.push(7); //7 1 5
+      s.pop(); //1 5
+      s.push(6); //6 1 5
+      s.pop(); //1 5
+    */
+      
+    //Exercise 1
     int i;
     for(i=1;i<argc;i++){
-      //  if(  x)  s.pop();
-        else s.push(atoi(argv[i]));
+      if(strcmp(argv[i], "x")==0) s.pop();
+      else s.push(atoi(argv[i]));
     }
-     */
-
-
-
    return 0;
 }
